d92cd95c0ba0be1a28ddd75d5bb3b3223b9ff3ef
Daisy Choi <chj15937@gmail.com>
2025-03-06 19:40:36 +1300

    fetch single character data

--- client/components/DisneyCharacter.tsx
+++ client/components/DisneyCharacter.tsx
@@ -1,15 +1,17 @@
-import { useDisneyCharacters } from '../apiClient'
-import { DisneyCollection } from '../../models/DisneyCharacters'
+import { useDisneyCharacter } from '../apiClient'
 
 export default function DisneyCharacter() {
-  const disneyCharacters = useDisneyCharacters()
+  const disneyCharacters = useDisneyCharacter(112)
   if (disneyCharacters.isError) return <p>ppending</p>
   if (disneyCharacters.isPending) return <p>ppending</p>
+//could't get the data.name. will solve tomorrow
+if(disneyCharacters.data) {
   console.log(disneyCharacters.data.data)
+
+  
 
   return (
     <>
-      <p>hi</p>
     </>
-  )
+  )}
 }
