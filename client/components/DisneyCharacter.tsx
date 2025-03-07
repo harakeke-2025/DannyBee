8c11916f9926674e9be28cfc2131dd0fa8cdebdf
Daisy Choi <chj15937@gmail.com>
2025-03-07 15:09:37 +1300

    updates

--- client/components/DisneyCharacter.tsx
+++ client/components/DisneyCharacter.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
-import { useDisneyCharacter, useDisneyCharacters } from '../apiClient'
+import { useDisneyCharacter } from '../apiClient'
 import LoadingPage from './LoadingPage'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function DisneyCharacter() {
   // const disneyCollection = useDisneyCharacters()
@@ -12,11 +12,12 @@ export default function DisneyCharacter() {
   const disneyCharacters = useDisneyCharacter(Number(id))
   const name = disneyCharacters.data?.data.name
 
-  const [disneyCharacter, setDisneyCharacter] = useState(name || '')
   //This is where the users input will be displayed
   const [guessingArr, setGuessingArr] = useState([])
-  // set the initial state to empty
-  const [input, setInput] = useState()
+
+  const [isReveal, setIsReveal] = useState(false)
+
+  const [funnyText, setFunnyText] = useEffect()
 
   if (disneyCharacters.isError) return <p data-testid="error">error</p>
   if (disneyCharacters.isPending) return <LoadingPage />
@@ -25,90 +26,107 @@ export default function DisneyCharacter() {
 
   //could't get the data.name. will solve tomorrow
   // if (disneyCharacters.data) {
+  if (disneyCharacters.data) {
+    const imageUrl = disneyCharacters.data.data.imageUrl
 
-  const imageUrl = disneyCharacters?.data.data.imageUrl
+    // Keyboard inputs
+    const alphabet = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+    ]
 
-  // Keyboard inputs
-  const alphabet = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ]
+    //handle user input, add character to guessing Array
+    const handleInputChange = (letter: string) => {
+      setGuessingArr((prevGuessedLetter) => [...prevGuessedLetter, letter])
 
-  //handle user input, add character to guessing Array
-  const handleInputChange = (letter: string) => {
-    setGuessingArr((prevGuessedLetter) => [...prevGuessedLetter, letter])
-    console.log(guessingArr)
-    // setGuessingArr(...letter.target.value)
-  }
-
-  const checkForWin = () => {
-    if (name.toLowerCase() === input.toLocaleLowerCase()) {
-      alert('YOU GOT THE SPELLING BEEEEE')
-    } else {
-      alert('try again'!)
+      // setGuessingArr(...letter.target.value)
     }
-  }
 
-  return (
-    <>
-      <div>
-        <h1>{name}</h1>
 
-        <img src={imageUrl} alt={name || 'Image'} />
+
+    const checkForWin = () => {
+      const arrayToString = guessingArr.join('')
+      console.log(arrayToString)
+      console.log(name)
+      if (arrayToString === name?.toLowerCase()) {
+        alert('YOU GOT THE DANNY BEEEEEEEEEE')
+        setIsReveal(true)
+      } else {
+        alert('try again'!)
+      }
+    }
+
+    function buildWord(letters, targetLength) {
+      for (const letter of letters) {
+        currentWord += letter
+        if (currentWord.length === targetLength) {
+          return currentWord //Return the word when it reaches the target length
+        }
+      }
+      return currentWord //Return the word even if it doesn't reach the target length
+    }
+
+    return (
+      <>
+      <div> 
+
+
       </div>
-      <div className="flex flex-row">
-        {guessingArr.map((item, index) => (
-          <p
-            className="lg:text-xx1 sm:text-md px-2 md:text-xl"
-            key={index}
-          >{`${item}  `}</p>
-        ))}
-      </div>
-      <div>
-        {alphabet.map((letter) => (
+        <div>
+          <h1>{isReveal ? name : 'Guess the character'}</h1>
+
+          <img src={imageUrl} alt={name || 'Image'} />
+        </div>
+        <div className="flex flex-row">
+          {guessingArr.map((item, index) => (
+            <p
+              className="lg:text-xx1 sm:text-md px-2 md:text-xl"
+              key={index}
+            >{`${item}  `}</p>
+          ))}
+        </div>
+        <div>
+          {alphabet.map((letter) => (
+            <button
+              key={letter}
+              className="rbg-blue-500 text-blue rounded border border-blue-700 px-4 py-2 font-bold hover:bg-blue-700"
+              onClick={() => handleInputChange(letter)}
+            >
+              {letter}
+            </button>
+          ))}
           <button
-            key={letter}
             className="rbg-blue-500 text-blue rounded border border-blue-700 px-4 py-2 font-bold hover:bg-blue-700"
-            onClick={() => handleInputChange(letter)}
+            onClick={checkForWin}
           >
-            {letter}
+            Check 4 win!
           </button>
-        ))}
-      </div>
-    </>
-  )
+        </div>
+      </>
+    )
+  }
 }
-
-// function buildWord(letters, targetLength) {
-//   for (const letter of letters) {
-//     currentWord += letter;
-//     if (currentWord.length === targetLength) {
-//       return currentWord; //Return the word when it reaches the target length
-//     }
-//   }
-//   return currentWord; //Return the word even if it doesn't reach the target length
-// }
